65be523e43798080daa43640bde7b64b8088ea7b
Tinxo <tinchoferrarigd@gmail.com>
2024-08-15 07:35:57 -0300

    web page

--- mlmodel.py
+++ mlmodel.py
@@ -1,17 +1,17 @@
-import pandas as pd
+# import pandas as pd
 
-import funciones as func
+# import funciones as func
 
-df = pd.read_parquet('google_yelp.parquet',engine='pyarrow')
+# df = pd.read_parquet('google_yelp.parquet',engine='pyarrow')
 
-# Cargar múltiples objetos desde un archivo .pkl
-# with open('artefact.pkl', 'rb') as archivo:
-#     artefactos = joblib.load(archivo)
+# # Cargar múltiples objetos desde un archivo .pkl
+# # with open('artefact.pkl', 'rb') as archivo:
+# #     artefactos = joblib.load(archivo)
 
 
-def rec_system(text="i would like to have some burguers"):
-    df_similarity = func.similaridad(df,text)
-    df_rec = func.recomendar_ciudad(df_similarity)
-    city = df_rec[df_rec["similarity_score"]==df_rec["similarity_score"].max()]["city"][0]
-    city.replace(" ","-")
-    return city
+# def rec_system(text="i would like to have some burguers"):
+#     df_similarity = func.similaridad(df,text)
+#     df_rec = func.recomendar_ciudad(df_similarity)
+#     city = df_rec[df_rec["similarity_score"]==df_rec["similarity_score"].max()]["city"][0]
+#     city.replace(" ","-")
+#     return city
